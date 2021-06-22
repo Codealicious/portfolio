026fc043d24db5cc93cb8384703fbfa8a9615b3c
Alex Hanson <alexhanson@Alexs-MacBook-Air.local>
2021-06-21 22:47:04 -0700

    .

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,8 +4,6 @@ const About = () => {
 
         let lists = document.querySelectorAll(".list__container");
 
-        console.log("click event being processed...");
-
         lists.forEach((list) => {
             if(list.className.includes("selected"))
                 list.classList.remove("list--selected");
@@ -16,36 +14,40 @@ const About = () => {
 
     return ( 
         <section className="about">
-            <div onClick={toggle} className="list__button">
-                &rarr;
+            <div className="img-container">
+                <div className="img-container__top">
+                    <img className="img-container__img img-container__img--1" src="./assets/images/wgu.png" alt=""/>
+                </div>
+                <div className="img-container__bottom">
+                    <img className="img-container__img img-container__img--2" src="./assets/images/ga.png" alt=""/>
+                </div>
             </div>
-            <div className="img-container img-container--top">
-                <img src="./assets/images/wgu.png" alt="" className="img" />
-            </div>
-            <div className="img-container img-container--bottom">
-                <img src="./assets/images/ga.png" alt=""/>
-            </div>
-            <div className="list__container dev list--selected">
-                <h2 className="list__headline dev__headline">Tools / Frameworks</h2>
-                <ul className="list">
-                    <li>React</li>
-                    <li>Sass</li>
-                    <li>Git</li>
-                    <li>Node.<span>js</span></li>
-                    <li>Django</li>
-                </ul>
-            </div>
-            <div className="list__container languages">
-                <h2 className="list__headline languages__headline">Languages</h2>
-                <ul className="list">
-                    <li>Python</li>
-                    <li>CSS3</li>
-                    <li>HTML</li>
-                    <li>Java</li>
-                    <li>C</li>
-                    <li >C++</li>
-                    <li>JavaScript</li>
-                </ul>
+            <div className="lists">
+                <div onClick={toggle} className="lists__button">
+                    &rarr;
+                </div>
+                <div className="list__container list--selected">
+                    <h2 className="list__headline dev">Tools / Frameworks</h2>
+                    <ul className="list">
+                        <li>React</li>
+                        <li>Sass</li>
+                        <li>Git</li>
+                        <li>Node.<span>js</span></li>
+                        <li>Django</li>
+                    </ul>
+                </div>
+                <div className="list__container">
+                    <h2 className="list__headline languages">Languages</h2>
+                    <ul className="list">
+                        <li>Python</li>
+                        <li>CSS3</li>
+                        <li>HTML</li>
+                        <li>Java</li>
+                        <li>C</li>
+                        <li >C++</li>
+                        <li>JavaScript</li>
+                    </ul>
+                </div>
             </div>
         </section>
      );
