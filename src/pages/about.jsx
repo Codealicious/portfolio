52ac2e90ab8870097563079e6e9aa07454a03b28
Alex Hanson <alexhanson@Alexs-MacBook-Air.local>
2021-06-22 18:28:24 -0700

    adjust responsive settings

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -14,37 +14,40 @@ const About = () => {
 
     return ( 
         <section className="about">
-            <div className="img-container">
-                <div className="img-container__top">
-                    <img className="img-container__img img-container__img--1" src="./assets/images/wgu.png" alt=""/>
+            <div className="large-screen-wrapper">
+                <div className="img-container">
+                    <div className="img-container__top">
+                        <img className="img-container__img img-container__img--1" src="./assets/images/wgu.png" alt=""/>
+                    </div>
+                    <div className="img-container__bottom">
+                        <img className="img-container__img img-container__img--2" src="./assets/images/ga.png" alt=""/>
+                    </div>
                 </div>
-                <div className="img-container__bottom">
-                    <img className="img-container__img img-container__img--2" src="./assets/images/ga.png" alt=""/>
-                </div>
-            </div>
-            <div className="lists">
-                <span className="lists__button" onClick={toggle}>&rarr;</span>
-                <div className="list__container list--selected">
-                    <h2 className="list__headline dev">Tools / Frameworks</h2>
-                    <ul className="list">
-                        <li>React</li>
-                        <li>Sass</li>
-                        <li>Git</li>
-                        <li>Node.<span>js</span></li>
-                        <li>Django</li>
-                    </ul>
-                </div>
-                <div className="list__container">
-                    <h2 className="list__headline languages">Languages</h2>
-                    <ul className="list">
-                        <li>Python</li>
-                        <li>CSS3</li>
-                        <li>HTML</li>
-                        <li>Java</li>
-                        <li>C</li>
-                        <li >C++</li>
-                        <li>JavaScript</li>
-                    </ul>
+                <div className="lists">
+                    <div className="list__container list--selected">
+                        <span className="lists__button" onClick={toggle}>&rarr;</span>
+                        <h2 className="list__headline dev">Tools / Frameworks</h2>
+                        <ul className="list">
+                            <li>React</li>
+                            <li>Sass</li>
+                            <li>Git</li>
+                            <li>Node.<span>js</span></li>
+                            <li>Django</li>
+                        </ul>
+                    </div>
+                    <div className="list__container">
+                        <span className="lists__button" onClick={toggle}>&rarr;</span>
+                        <h2 className="list__headline languages">Languages</h2>
+                        <ul className="list">
+                            <li>Python</li>
+                            <li>CSS3</li>
+                            <li>HTML</li>
+                            <li>Java</li>
+                            <li>C</li>
+                            <li >C++</li>
+                            <li>JavaScript</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </section>
